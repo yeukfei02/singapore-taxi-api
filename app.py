17802f260260faa01f76c61d266c81ec31f215dc
Donald Wu <yeukfei02@gmail.com>
2021-05-05 21:50:19 +0800

    fix: add auth_routes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from chalice import Chalice
 from chalicelib.routes.main import main_routes
+from chalicelib.authorizer.authorizer import auth_routes
 from chalicelib.routes.user import user_routes
 from chalicelib.routes.favourites import favourites_routes
 from chalicelib.routes.taxi import taxi_routes
@@ -10,6 +11,7 @@ load_dotenv()
 
 app = Chalice(app_name='singapore-taxi-api')
 app.register_blueprint(main_routes)
+app.register_blueprint(auth_routes)
 app.register_blueprint(user_routes)
 app.register_blueprint(favourites_routes)
 app.register_blueprint(taxi_routes)
